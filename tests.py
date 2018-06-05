b2fe043c41136fb2f537d29bbe7b28a10e1124e6
Michail_Z <apxahrejl@gmail.com>
2018-06-05 08:25:40 +0500

    Style updating

--- tests.py
+++ tests.py
@@ -14,7 +14,13 @@ class TestPriceFormatter(unittest.TestCase):
                          )
 
     def test_uncorrect_data(self):
-        test_data_list = ['dfsd', '123,34', [1, 2, 3], {'key', 'value'}, (1, 2,), TestClass()]
+        test_data_list = [
+            'dfsd',
+            '123,34',
+            [1, 2, 3],
+            {'key', 'value'},
+            (1, 2,), TestClass()
+        ]
         for test_data in test_data_list:
             with self.assertRaises((TypeError, ValueError, NameError)):
                 format_price.format_price(test_data)
